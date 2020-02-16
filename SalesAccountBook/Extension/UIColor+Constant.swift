525a8af2c526aa78d52fb87dbe993f60ff4a1c6c
LabLamb <lablambworks@gmail.com>
2020-02-16 22:54:53 +0800

    Add temp theme

--- SalesAccountBook/Extension/UIColor+Constant.swift
+++ SalesAccountBook/Extension/UIColor+Constant.swift
@@ -6,9 +6,17 @@ import UIKit
 
 extension UIColor {
     // Plain
-    static let background = white
-    static let text = black
-    static let buttonIcon = systemBlue
-    static let primary = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha:1.0)
-    static let accent = lightGray
+//    static let background = white
+//    static let text = black
+//    static let buttonIcon = systemBlue
+//    static let primary = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha:1.0)
+//    static let accent = lightGray
+    
+    // Google
+    static let background = UIColor(red:0.13, green:0.13, blue:0.14, alpha:1.0)
+    static let text = white
+    static let buttonIcon = UIColor(red:0.85, green:0.08, blue:0.23, alpha:1.0)
+    static let primary = UIColor(red:0.16, green:0.17, blue:0.18, alpha:1.0)
+    static let accent = UIColor(red:0.7, green:0.7, blue:0.7, alpha:1.0)
+    
 }
