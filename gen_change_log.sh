f024d30b9a568cc81f25887d84349629d99e2270
LabLamb <lablambworks@gmail.com>
2020-03-07 18:25:08 +0800

    Update CHANGELOG Script

--- gen_change_log.sh
+++ gen_change_log.sh
@@ -1,1 +1,1 @@
-github_changelog_generator -u LabLamb -p KalKa-iOS -t aa8fd3c4b9a6e4c8367566c3a3582cf81cf0253b
+github_changelog_generator -u LabLamb -p KalKa-iOS -t 0605b63920d4880104700a33ee383f29b75e220a
