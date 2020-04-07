5d88b74d372ecd7300aba6012a5a154bb00e9b58
LabLamb <lablambworks@gmail.com>
2020-04-07 20:16:25 +0800

    Config visuals for membership screen

--- KalKa/UI/Membership Screen/MembershipViewController.swift
+++ KalKa/UI/Membership Screen/MembershipViewController.swift
@@ -4,4 +4,27 @@
 
 import SnapKit
 
-class MembershipViewController: UIViewController {}
+class MembershipViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = .background
+        self.navigationItem.title = .membership
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(self.saveSettings))
+        self.setup()
+    }
+    
+    private func setup() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.tintColor = .buttonIcon
+        if var textAttributes = navigationController?.navigationBar.titleTextAttributes {
+            textAttributes[NSAttributedString.Key.foregroundColor] = UIColor.text
+            navigationController?.navigationBar.titleTextAttributes = textAttributes
+        } else {
+            let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.text]
+            navigationController?.navigationBar.titleTextAttributes = textAttributes
+        }
+    }
+    
+}
